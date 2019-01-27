bc7842335d8135fb73f4b48da8196fa4e717f2be
Taeho Kim <tkim@lyft.com>
2019-01-27 05:20:50 -0800

    Add NoteEditViewController

---
+++ Taeho-iOS/NoteEditViewController.swift
@@ -0,0 +1,21 @@
+//
+//  NoteEditViewController.swift
+//  Taeho-iOS
+//
+//  Created by Taeho Kim on 1/27/19.
+//  Copyright © 2019 taeho.io. All rights reserved.
+//
+
+import UIKit
+import YYText
+
+class NoteEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationItem.largeTitleDisplayMode = .never
+        self.navigationController?.hidesBarsOnSwipe = true
+    }
+
+}
